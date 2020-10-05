065b65049f46642aafde49aee5e93acab42bd67a
Camila Nepomuceno <camila.cno@gmail.com>
2020-10-04 22:15:10 -0300

    Models loader

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
-import { Router } from 'express';
+import { Router } from "express";
+
+import User from "./app/models/User";
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello Beth Cabelo e Estética' });
+routes.get("/", (req, res) => {
+  res.json({ message: "Hello Beth Cabelo e Estética" });
 });
 
 export default routes;
