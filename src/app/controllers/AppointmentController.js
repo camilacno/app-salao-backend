f8607c6c5b252780ea0b436ebafb719cf31db716
Camila Nepomuceno <camila.cno@gmail.com>
2020-10-10 17:16:00 -0300

    Email templates config

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -124,6 +124,11 @@ class AppointmentController {
           as: "provider",
           attributes: ["name", "email"],
         },
+        {
+          model: User,
+          as: "user",
+          attributes: ["name"],
+        },
       ],
     });
 
@@ -147,8 +152,15 @@ class AppointmentController {
     // Mail send
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: "Appointment cancellation",
-      text: "You have a cancellation in one of your appointments.",
+      subject: "Agendamento cancelado",
+      template: "cancellation",
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
