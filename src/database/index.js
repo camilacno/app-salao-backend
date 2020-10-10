e0798b348b5bd32c7d4e1eb046cbf7c04b7dd0d7
Camila Nepomuceno <camila.cno@gmail.com>
2020-10-10 02:33:25 -0300

    MongoDB config

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from "sequelize";
+import mongoose from "mongoose";
 
 import User from "../app/models/User";
 import File from "../app/models/File";
@@ -10,6 +11,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -20,6 +22,16 @@ class Database {
       .map(
         (model) => model.associate && model.associate(this.connection.models)
       );
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      "mongodb://192.168.99.100:27017/salao",
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+      }
+    );
   }
 }
 
